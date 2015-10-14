aeb2e81680d374cb2f019ae3db4da36933aec6c6
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-10-14 08:03:16 +0200

    Make cargo test pass and add some documentation

--- examples/test.rs
+++ examples/test.rs
@@ -1,11 +1,4 @@
 extern crate immi;
 
 fn main() {
-    let layout = .draw();
-
-    //let layout = layout.margin(1.0, 1.0, 1.0, 1.0);
-
-    for layout in layout.equal_vertical_split(5) {
-
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//!
+//!
+//! # Drawing
+//!
+//! Once you have a `DrawContext` you can start drawing your user interface.
+//!
+//! A `DrawContext` represents an area of the viewport where things you should be drawn. Initially
+//! this area contain the whole viewport, but you can call methods on the `DrawContext` to adjust
+//! this area.
+//!
+//! Example:
+//!
+//! ```rust
+//! fn draw_ui<D>(draw: immi::DrawContext<D>) where D: immi::Draw<ImageResource = str> {
+//!     // draws an image on the whole screen
+//!     // the bottom alignment is used if the aspect ratio of the image doesn't match the aspect
+//!     // ratio of the viewport
+//!     immi::widgets::image::draw(&draw, "background", &immi::Alignment::bottom());
+//!
+//!     // we resize the viewport so that it only covers the top half of the screen
+//!     let draw = draw.vertical_rescale(0.5, &immi::VerticalAlignment::Top);
+//!
+//!     // draws an image on the top half of the screen
+//!     immi::widgets::image::draw(&draw, "top_background", &immi::Alignment::center());
+//! }
+//! ```
+//!
 #[macro_use]
 extern crate lazy_static;
 extern crate time;
@@ -17,5 +44,3 @@ mod matrix;
 
 pub mod animations;
 pub mod widgets;
-
-
