767803f72c34c39a7afafcc54103057168bcd184
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-11-05 13:49:06 +0100

    Fixed aspect ratio not being taken into account

--- src/widgets/image9.rs
+++ src/widgets/image9.rs
@@ -30,9 +30,9 @@ pub fn draw<D: ?Sized + Draw>(draw: &DrawContext<D>, left_border_percent: f32,
     assert!(top_percent + bottom_percent <= 1.0);
     assert!(left_percent + right_percent <= 1.0);
 
-    let top_border_percent = left_border_percent * top_percent / left_percent;
-    let right_border_percent = top_border_percent * right_percent / top_percent;
-    let bottom_border_percent = right_border_percent * bottom_percent / right_percent;
+    let top_border_percent = left_border_percent * top_percent / left_percent / draw.width_per_height();
+    let right_border_percent = top_border_percent * right_percent / top_percent * draw.width_per_height();
+    let bottom_border_percent = right_border_percent * bottom_percent / right_percent / draw.width_per_height();
 
     assert!(top_border_percent + bottom_border_percent <= 1.0);
     assert!(left_border_percent + right_border_percent <= 1.0);
