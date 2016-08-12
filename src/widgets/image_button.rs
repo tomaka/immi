62f000be5e6b8a32be3e26b6b6ae48462a99a03c
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-08-12 11:53:03 +0200

    Make the user manually pass the UiState

--- src/widgets/image_button.rs
+++ src/widgets/image_button.rs
@@ -1,32 +1,34 @@
 use Alignment;
 use Draw;
 use DrawContext;
+use UiState;
 
 use widgets::Interaction;
 
-pub fn draw<D: ?Sized + Draw>(draw: &DrawContext<D>, normal_image: &D::ImageResource,
-                              hovered_image: &D::ImageResource, active_image: &D::ImageResource,
-                              alignment: &Alignment) -> Interaction
+pub fn draw<D: ?Sized + Draw>(draw: &DrawContext<D>, ui_state: &mut UiState,
+                              normal_image: &D::ImageResource, hovered_image: &D::ImageResource,
+                              active_image: &D::ImageResource, alignment: &Alignment)
+                              -> Interaction
 {
     let ratio = draw.draw().get_image_width_per_height(normal_image);
-    stretch(&draw.enforce_aspect_ratio_downscale(ratio, alignment), normal_image, hovered_image,
-                                                 active_image)
+    stretch(&draw.enforce_aspect_ratio_downscale(ratio, alignment), ui_state, normal_image,
+            hovered_image, active_image)
 }
 
-pub fn stretch<D: ?Sized + Draw>(draw: &DrawContext<D>, normal_image: &D::ImageResource,
-                                 hovered_image: &D::ImageResource, active_image: &D::ImageResource)
-                                 -> Interaction
+pub fn stretch<D: ?Sized + Draw>(draw: &DrawContext<D>, ui_state: &mut UiState,
+                                 normal_image: &D::ImageResource, hovered_image: &D::ImageResource,
+                                 active_image: &D::ImageResource) -> Interaction
 {
     let widget_id = draw.reserve_widget_id();
 
     if draw.is_cursor_hovering() {
         draw.set_cursor_hovered_widget();
 
-        if Some(widget_id.clone()) == draw.get_active_widget() {
+        if Some(widget_id.clone()) == ui_state.active_widget {
             draw.draw().draw_image(active_image, draw.matrix());
 
             if draw.cursor_was_released() {
-                draw.clear_active_widget();
+                ui_state.active_widget = None;
                 Interaction::Clicked
             } else {
                 Interaction::None
@@ -34,7 +36,7 @@ pub fn stretch<D: ?Sized + Draw>(draw: &DrawContext<D>, normal_image: &D::ImageR
 
         } else if draw.cursor_was_pressed() {
             draw.draw().draw_image(active_image, draw.matrix());
-            draw.write_active_widget(widget_id.clone());
+            ui_state.active_widget = Some(widget_id.clone());
             Interaction::None
 
         } else {
