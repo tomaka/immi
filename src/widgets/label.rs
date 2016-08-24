6545774c42556aaeceb669e3ec61280ed8a055d2
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-08-24 22:44:14 +0200

    Add some documentation for the labels

--- src/widgets/label.rs
+++ src/widgets/label.rs
@@ -6,40 +6,12 @@ use DrawContext;
 use HorizontalAlignment;
 use matrix::Matrix;
 
-pub fn contain<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
-                                 alignment: &Alignment)
-{
-    helper(draw, text_style, text, |ratio| {
-        let draw = draw.enforce_aspect_ratio_downscale(ratio, alignment);
-
-        if !draw.cursor_hovered_widget() {
-            if draw.is_cursor_hovering() {
-                draw.set_cursor_hovered_widget();
-            }
-        }
-
-        draw.matrix().clone()
-    })
-}
-
-pub fn cover<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
-                               alignment: &Alignment)
-{
-    helper(draw, text_style, text, |ratio| {
-        let draw = draw.enforce_aspect_ratio_upscale(ratio, alignment);
-
-        if !draw.cursor_hovered_widget() {
-            if draw.is_cursor_hovering() {
-                draw.set_cursor_hovered_widget();
-            }
-        }
-
-        draw.matrix().clone()
-    })
-}
-
-/// The text will use the current height and will stretch horizontally as needed to preserve the
-/// correct aspect ratio.
+/// Draws text. The text will always have the same height as the context and will stretch
+/// horizontally as needed to have a correct aspect ratio.
+///
+/// This is usually the function that you want in order to draw text. Even though the text
+/// can overflow its container if it is too long, it is usually visually better to have an
+/// overflow than to have multiple texts of different heights when they should be the same.
 pub fn flow<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
                               alignment: &HorizontalAlignment)
 {
@@ -57,7 +29,41 @@ pub fn flow<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, 
     })
 }
 
+/// Draws text. The text will be sized so that it is entirely contained within the context, and
+/// either its width or its height is equal to the width or the height of the context.
+pub fn contain<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
+                                 alignment: &Alignment)
+{
+    helper(draw, text_style, text, |ratio| {
+        let draw = draw.enforce_aspect_ratio_downscale(ratio, alignment);
 
+        if !draw.cursor_hovered_widget() {
+            if draw.is_cursor_hovering() {
+                draw.set_cursor_hovered_widget();
+            }
+        }
+
+        draw.matrix().clone()
+    })
+}
+
+/// Draws text. The text will be sized so that it entirely covers the context, and either its
+/// width or its height is equal to the width or the height of the context.
+pub fn cover<D: ?Sized + Draw>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
+                               alignment: &Alignment)
+{
+    helper(draw, text_style, text, |ratio| {
+        let draw = draw.enforce_aspect_ratio_upscale(ratio, alignment);
+
+        if !draw.cursor_hovered_widget() {
+            if draw.is_cursor_hovering() {
+                draw.set_cursor_hovered_widget();
+            }
+        }
+
+        draw.matrix().clone()
+    })
+}
 
 fn helper<D: ?Sized + Draw, F>(draw: &DrawContext<D>, text_style: &D::TextStyle, text: &str,
                                final_matrix: F)
